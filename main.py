5df9eb0d14f7840953a02c3d92206c056717a71c
Khandha <wojciech.swiecicki1@gmail.com>
2022-03-26 10:45:09 +0100

    Added tkinter - choosing files

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import os
+
 from greedy import Greedy
 from graph_generator import generate_graph as chart
 # from generator import Generator
+import tkinter as tk
+from tkinter import filedialog
+from os import getcwd
 
 
 array = []
-with open('m50n200.txt') as f:
+
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select file", filetypes=[("Text Files", "*.txt")])
+with open(file_path) as f:
     for line in f:
         array.append(int(line.rstrip()))
 processor_count = array.pop(0)
